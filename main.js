e4454fed6c7f0a21aed8e69aece98e466f8f30ea
Manan0604 <84577663+Manan0604@users.noreply.github.com>
2022-01-22 10:00:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function dogandcat(){
+    window.location="dogandcat.html"
+}
+
+function storeroom(){
+    window.location="storeroom.html"
+}
+
+function bedroom(){
+    window.location="bedroom.html"
+}
+
+function bathroom(){
+    window.location="bathroom.html"
+}
+
+function laptop(){
+    window.location="laptop.html"
+}
